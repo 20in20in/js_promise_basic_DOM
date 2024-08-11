ae935430077651222508b3c36311992949d30d75
Igor Novikov <novikov.igor.20@gmail.com>
2024-08-11 22:31:41 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,26 @@
+"use strict";
+const logo = document.querySelector(".logo");
+const promiseOne = new Promise((resolve, reject)=>{
+    logo.addEventListener("click", ()=>{
+        resolve("Promise was resolved!");
+    });
+});
+const promiseTwo = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        reject(new Error("Promise was rejected!"));
+    }, 3000);
+});
+const resultHandler = (message, isError = false)=>{
+    const resultMessage = document.createElement("div");
+    resultMessage.className = isError ? "message error-message" : "message";
+    resultMessage.textContent = message;
+    document.body.appendChild(resultMessage);
+};
+promiseOne.then((message)=>resultHandler(message)).catch((error)=>{
+    resultHandler(error.message, true);
+});
+promiseTwo.then((message)=>resultHandler(message)).catch((error)=>{
+    resultHandler(error.message, true);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
